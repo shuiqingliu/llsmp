906f101271ecf1522f272185f0ee76e291791866
qingliu <shuiqingliu14@gmail.com>
2017-07-10 15:50:06 +0800

    add: install litespeed>check_version

---
+++ addons/litespeed.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+local VERSION
+
+check_version(){
+ 
+    ACTION=    
+    rm release >/dev/null 2>&1    
+    wget -O release http://open.litespeedtech.com/packages/release
+    last_version=`cat release`
+    if [[ -d $SERVER_DIR ]]; then
+
+        if [[ $last_version == `cat $SERVER_DIR/VERSION` ]]; then
+              echogreen "You already installed the last version of llsmp"
+              while true; do
+              read -p "Do you wish to reinstall this program[y/n]?" yn
+              case $yn in
+                  [Yy]* ) ACTION=reinstall; break;;
+                  [Nn]* ) exit;;
+                  * ) echo "Please answer yes or no.";;
+              esac
+        else
+            #TODO:upgrade
+            ACTION=update    
+        fi
+    else 
+        #TODO: intall a new llsmp   
+        ACTION=install
+    fi
+
+}
+
+centos_litespeed(){
+
+    check_verson
+    if [[ $ACTION == "install" ]]; then
+    
+}
+
