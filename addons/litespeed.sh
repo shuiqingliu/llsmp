4cd4adc3d8c104c55f95cef88bc97e05bd12270d
qingliu <shuiqingliu14@gmail.com>
2017-07-11 14:55:18 +0800

    add: debian install litespeed

--- addons/litespeed.sh
+++ addons/litespeed.sh
@@ -36,8 +36,29 @@ centos_litespeed(){
     #install hashlib
     yum -y install python-hashlib
     yum -y $ACTION epel-release
+    #install litespeed repository
     rpm -Uvh http://rpms.litespeedtech.com/centos/litespeed-repo-1.1-1.el$OS_VERSION.noarch.rpm 
     yum -y $ACTION openlitespeed
 
 }
 
+debian_litespeed(){
+    
+    check_verson
+    if [[ $ACTION == "reinstall" ]]; then
+        $ACTION="--reinstall"
+    elif [[ $ACTION == "update" ]]; then
+        $ACTION="--only-upgrade"
+    else
+        $ACTION=""
+    fi
+    #add litespeed repository
+    echo "deb http://rpms.litespeedtech.com/debian/ $OSVER main"  > /etc/apt/sources.list.d/lst_debian_repo.list 
+    #add litespeed gpg key
+    wget -O /etc/apt/trusted.gpg.d/lst_debian_repo.gpg http://rpms.litespeedtech.com/debian/lst_debian_repo.gpg
+    wget -O /etc/apt/trusted.gpg.d/lst_repo.gpg http://rpms.litespeedtech.com/debian/lst_repo.gpg
+    #install litespeed
+    apt-get -y update
+    apt-get -y install $ACTION openlitespeed
+
+}
