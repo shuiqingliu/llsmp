5dddb92708893fa3d369dc0eb1751a7e374b0ea3
qingliu <shuiqingliu14@gmail.com>
2017-07-18 23:20:57 +0800

    add: install php on debain and ubuntu

--- addons/php.sh
+++ addons/php.sh
@@ -12,3 +12,30 @@ centos_php(){
     ln -sf $SERVER_DIR/lsphp$PHPVER/bin/lsphp
     $SERVER_DIR/fcgi-bin/lsphp5
 }
+
+debian_php(){
+
+    #check action 
+    if [[ $ACTION == "reinstall" ]]; then
+        $ACTION="--reinstall"
+    elif [[ $ACTION == "update" ]]; then
+        $ACTION="--only-upgrade"
+    else
+        $ACTION=""
+    fi
+    #install base package
+    apt-get -y install $ACTION lsphp$PHPVER lsphp$PHPVER-imap  
+    #install special package with php version 70 or 71
+    if [[ "x$PHPVER" != "x70" && "x$PHPVER" != "x71" ]]; then
+            apt-get -y install $ACTION lsphp$PHPVER-gd
+            lsphp$PHPVER-mcrypt
+    else
+            apt-get -y install $ACTION lsphp$PHPVER-common
+    fi
+    
+    #creat soft link
+    ln -sf $SERVER_DIR/lsphp$PHPVER/bin/lsphp
+    $SERVER_DIR/fcgi-bin/lsphp5
+
+
+}
