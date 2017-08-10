fe0f195b2f902fd583bfe2ee33d3a2e77c2a7216
qingliu <shuiqingliu14@gmail.com>
2017-08-10 11:07:43 +0800

    fix:source error

--- addons/php.sh
+++ addons/php.sh
@@ -3,7 +3,9 @@
 #install php for llsmp
 centos_php(){
     #include check action
-    source litespeed.sh
+
+    current_dir="$(dirname "$0")"
+    source $current_dir/litespeed.sh
     yum -y $ACTION lsphp$PHPVER lsphp$PHPVER-common lsphp$PHPVER-gd
     lsphp$PHPVER-process lsphp$PHPVER-mbstring lsphp$PHPVER-xml
     lsphp$PHPVER-mcrypt lsphp$PHPVER-pdo lsphp$PHPVER-imap
