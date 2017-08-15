f424c1bce8adc7ed7647f0cdc0479ac339970dd2
qingliu <shuiqingliu14@gmail.com>
2017-08-15 23:52:00 +0800

    add:install/uninstall usage

--- addons/llsmp.sh
+++ addons/llsmp.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 usage(){
-
-
+    
+    echo "USAGE :                $0 [optins] [optins]"
+    echo "OPTIONS                                    "
+    echo "--install(-i) [ioCube|redis|memcached|ftp...]  To install some feature componets."
+    echo "--uninstall(-r) [all|php|mysql|litespeed]      To uninstall llsmp componets or remove it all"
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
+    echo "--install(-i) [ioCube|Pure-FTPd|memcached...]  To install some feature componets."
 
 }
 #================component=================
