f3e893645103d745c4a15cd594d757258bda2f16
qingliu <shuiqingliu14@gmail.com>
2017-08-17 22:56:50 +0800

    add:vhost todo

--- addons/llsmp.sh
+++ addons/llsmp.sh
@@ -4,40 +4,46 @@ usage(){
     
     echo "USAGE :                $0 [optins] [optins]"
     echo "OPTIONS                                    "
-    echo "--install(-i) [ioCube|redis|memcached|ftp...]  To install some feature componets."
-    echo "--uninstall(-r) [all|php|mysql|litespeed]      To uninstall llsmp componets or remove it all."
-    echo "vhost         [add|del]                        To add/remove vhost."
-    echo "lsws          [start|restart|stop]             To manage litespeed."
-    echo "mysql         [start|stop|restart]             To manage mysql."
-    echo "pureftpd      [start|stop|restart]             To manage ftp."
+    echo "--install(-i)   [ioCube|redis|memcached|ftp...]  To install some feature componets."
+    echo "--uninstall(-r) [all|php|mysql|litespeed]        To uninstall llsmp componets or remove it all."
+    echo "vhost           [add|del]                        To add/remove vhost."
+    echo "lsws            [start|restart|stop]             To manage litespeed."
+    echo "mysql           [start|stop|restart]             To manage mysql."
+    echo "pureftpd        [start|stop|restart]             To manage ftp."
 
 }
 #================component=================
 
 lsws(){
-
+        :
 }
 
 vhost(){
-
+    local ACTION=$1
+    if [[ "x$ACTION" == "xadd" ]];then
+        printf "Please input your site name:"
+        read site
+        if [[ -e  ]];then
+        fi
+    fi
 }
 
 mysql(){
-
+:
 
 }
 
 ftp(){
-
+:
 }
 
 memcached(){
-
+:
 
 }
 
 redis(){
-
+:
 }
 
-
+usage
